b26eb0c039ed16bb67587b46cce86905f8ae2b65
JustAgust <charles.sparks@atlasschool.com>
2024-04-03 16:11:03 -0500

    added nop prototype to .h and made nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing, which is still something.
+ * @stack: Double pointer to head of stack.
+ * @line_number: The line number in stack
+*/
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void) stack;
+    (void) line_number;
+}
