80002b0603a56c1d5cf84eaa7d5f2724b3b98f99
JustAgust <charles.sparks@atlasschool.com>
2024-04-03 16:03:20 -0500

    added push, pall prototypes

--- monty.h
+++ monty.h
@@ -35,3 +35,6 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
