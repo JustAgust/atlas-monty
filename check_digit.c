79cebb1d762e503a34cf1404fbf88035cc9aa96c
JustAgust <charles.sparks@atlasschool.com>
2024-04-06 11:50:18 -0500

    fixing Betty code style check_digit

--- check_digit.c
+++ check_digit.c
@@ -4,19 +4,19 @@
  * check_digit - checks if string is a digit
  * @token: string to check
  * Return: 0 if success, 1 if not
-*/
+ */
 int check_digit(char *token)
 {
-    int i = 0;
+	int i = 0;
 
-    if (token == NULL)
-    return (1);
-    if (token[0] == '-')
-    i = 1;
-    for (; token[i] != '\0'; i++)
-    {
-        if (isdigit(token[i]) == 0)
-        return (1);
-    }
-    return (0);
+	if (token == NULL)
+		return (1);
+	if (token[0] == '-')
+		i = 1;
+	for (; token[i] != '\0'; i++)
+	{
+		if (isdigit(token[i]) == 0)
+			return (1);
+	}
+	return (0);
 }
