e9e40efb6e39ffff48be617fe3131ca192c3111b
JustAgust <charles.sparks@atlasschool.com>
2024-04-06 14:55:18 -0500

    still coding style

--- pint.c
+++ pint.c
@@ -4,15 +4,15 @@
  * pint - prints the value at the top of stack
  * @stack: pointer to top of stack
  * @line_number: line index
-*/
+ */
 void pint(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp = *stack;
+	stack_t *temp = *stack;
 
-    if (temp == NULL)
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", temp->n);
+	if (temp == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", temp->n);
 }
--- pop.c
+++ pop.c
@@ -5,17 +5,18 @@
  * @stack: pointer to stack
  * line_number: line index
  * Return: void
-*/
+ */
 void pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *s;
-    s = *stack;
+	stack_t *s;
 
-    if (s == NULL)
-    {
-        fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    *stack = s->next;
-    free(s);
+	s = *stack;
+
+	if (s == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = s->next;
+	free(s);
 }
