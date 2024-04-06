66ecf0ec48bde0eac945ac4e42e78923ab607684
JustAgust <charles.sparks@atlasschool.com>
2024-04-06 14:59:27 -0500

    coding style betty

--- swap.c
+++ swap.c
@@ -5,22 +5,22 @@
  * @stack: pointer to stack
  * @line_number: line index
  * Return: void
-*/
+ */
 void swap(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp = NULL;
+	stack_t *temp = NULL;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    temp = (*stack)->next;
-    (*stack)->next = temp->next;
-    if (temp->next != NULL)
-    temp->next->prev = *stack;
-    temp->prev = NULL;
-    temp->next = *stack;
-    (*stack)->prev = temp;
-    *stack = temp;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = *stack;
+	temp->prev = NULL;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = temp;
 }
