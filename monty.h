e1e3a872f6f7e5d30f61b37cd1cfe1174f649d04
JustAgust <charles.sparks@atlasschool.com>
2024-04-06 09:30:02 -0500

    added prototypes to main.h

--- monty.h
+++ monty.h
@@ -38,7 +38,13 @@ typedef struct instruction_s
 
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 
+int exec_handler(char *opcode, stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *head);
+void free_all(char *line, stack_t *stack, FILE *file);
 #endif
