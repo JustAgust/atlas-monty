35da97c9d8f9396ea9508cfa8f4746a63d5642cc
JustAgust <charles.sparks@atlasschool.com>
2024-04-06 14:35:31 -0500

    removing spaces at start of line

--- nop.c
+++ nop.c
@@ -4,11 +4,11 @@
  * nop - Does nothing, which is still something.
  * @stack: Double pointer to head of stack.
  * @line_number: Line index
-*/
+ */
 
 void nop(stack_t **stack, unsigned int line_number)
 {
-    /*used as a place holder for no-op instruction*/
-    (void) stack;/*Suppress unused param*/
-    (void) line_number;/*Suppress unused param*/
+	/*used as a place holder for no-op instruction*/
+(void) stack;/*Suppress unused param*/
+(void) line_number;/*Suppress unused param*/
 }
--- pall.c
+++ pall.c
@@ -5,17 +5,17 @@
  * @stack: pointer to top of stack
  * @line_number: Line index (unused)
  * Return: void
-*/
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *s;
+	stack_t *s;
 
-    (void)line_number;
-    s = *stack;
+	(void)line_number;
+	s = *stack;
 
-    while (s != NULL)
-    {
-        printf("%d\n", s->n);
-        s = s->next;
-    }
+	while (s != NULL)
+	{
+		printf("%d\n", s->n);
+		s = s->next;
+	}
 }
