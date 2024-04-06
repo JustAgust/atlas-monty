662498338bb0381cda68d95ecc6cda13f1898076
JustAgust <charles.sparks@atlasschool.com>
2024-04-06 09:20:34 -0500

    added function exec_handler to call opcodes and apply, added func free_ to handle clean up

---
+++ exec_handler.c
@@ -0,0 +1,34 @@
+ #include "monty.h"
+ /**
+  * exec_handler - get the opcode
+  * @opcode: opcode
+  * @stack: pointer to the stack
+  * @line_number: index
+  * Return: 1 if success, 0 if opcode
+ */
+int exec_handler(char *opcode, stack_t **stack, unsigned int line_number)
+{
+    /*define array of instruct struct*/
+    instruction_t instructions[] = {
+        {"push", push},
+        {"pall", pall},
+        {"pint", pint},
+        {"pop", pop},
+        {"swap", swap},
+        {"add", add},
+        {"nop", nop},
+        {NULL, NULL},
+    }
+    int i = 0;
+    /*search for the opcode*/
+    while (instructions[i].opcode)
+    {
+        if (strcmp(opcode, instructions[i].opcode) == 0)
+        {
+            instructions[i].f(stack, line_number);
+            return (1);
+        }
+        i++
+    }
+    return (0);
+}
---
+++ free_stack.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * free_stack - frees the stack
+ * @head: pointer to the head of stack
+ * Return: void
+*/
+void free_stack(stack_t *head)
+{
+    stack_t *s;
+    s = head;
+    while (s)
+    {
+        s = s->next;
+        free(head);
+        head = s;
+    }
+}
+
+/**
+ * free_all - free all
+ * @line: line
+ * @stack: stack
+ * @file: file
+ * Return: void
+*/
+void free_all(char *line, stack_t *stack, FILE *file)
+{
+    free(line);
+    free_stack(stack);
+    fclose(file);
+}
