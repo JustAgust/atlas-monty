1063cdcd4cc16f77961b7c2626435d44b4e6a54b
JustAgust <charles.sparks@atlasschool.com>
2024-04-06 11:35:44 -0500

    pall.c created

---
+++ pall.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * pall - prints the stack
+ * @stack: pointer to top of stack
+ * @line_number: Line number (unused)
+ * Return: void
+*/
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *s;
+    (void)line_number;
+
+    s = *stack;
+
+    if (s)
+    while (s)
+    {
+        printf("%d\n", s->n);
+        s = s->next;
+    }
+}
